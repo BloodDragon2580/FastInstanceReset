b07bb16ef403ad2efd90b364fdd085174bf9f691
BloodDragon2580 <gewinner.thomas@gmail.com>
2023-01-05 07:29:28 +0100

    Upload v1.7

--- FastInstanceReset/FastInstanceReset.lua
+++ FastInstanceReset/FastInstanceReset.lua
@@ -1,8 +1,9 @@
 local frame=CreateFrame("Frame");
 local inInstance, instanceType = IsInInstance();
+local isGroup = IsInGroup();
 frame:RegisterEvent("PLAYER_ENTERING_WORLD");
 frame:SetScript("OnEvent",function(self,event,...)
-	if (inInstance == false and event == "PLAYER_ENTERING_WORLD") then
+	if (inInstance == false and isGroup == false and event == "PLAYER_ENTERING_WORLD") then
 		ResetInstances()
 	end
 end);
